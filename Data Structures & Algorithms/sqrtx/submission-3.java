a3c95bd8cf8aa6aff2f7d75bb23065c6b87e0b86
Rahul Dwivedi <47186162+rakshal123@users.noreply.github.com>
2026-06-08 23:26:59 +0530

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0) return 0;
+        int l = 1;
+        int r = x;
+        int realAns = 1;
+        while(l<=r) {
+            int mid = l+(r-l)/2;
+            int ans = x/mid;
+            if(mid <= ans) {
+                realAns = mid;
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+        return realAns;
+    }
+}
